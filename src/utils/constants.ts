aafc530f739db2bb7e57fe5cc345e2e3f7a7f5bb
TitanXtream <titanicking24@gmail.com>
2025-09-10 13:42:47 +0530

    Metadata added

---
+++ src/utils/constants.ts
@@ -0,0 +1,22 @@
+export const COMPANY_NAME = "Synkluna";
+
+export const APP_NAME = "Rag Studio";
+
+export const APP_DOMAIN = "https://ragstudio.synkluna.com";
+
+export const APP_INNER_DOMAIN =
+  "https://synkluna-rag-studio-web-app.vercel.app/";
+
+export const APP_DESCRIPTION =
+  "Build, test, and deploy Retrieval-Augmented Generation (RAG) workflows. RAG Studio lets you create LLM APIs, test flows, and launch AI apps in minutes.";
+
+export const openGraphImages = [
+  {
+    alt: `${COMPANY_NAME} logo`,
+    url: `${APP_INNER_DOMAIN}/opengraph/synkluna-rag-studio-opengraph-image.png`,
+  },
+  {
+    alt: `${COMPANY_NAME} logo`,
+    url: `${APP_INNER_DOMAIN}/opengraph/synkluna-rag-studio-opengraph-image.jpg`,
+  },
+];
